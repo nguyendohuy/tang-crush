c670f6981f46555d36f8382b5f0842ca8b9941b7
nguyendohuy <85340324+nguyendohuy@users.noreply.github.com>
2021-06-04 17:04:44 +0700

    Create config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Yêu bé gất nhìu ❤.',
+    mess: 'Anh biết mà 🥰. Yêu em nhìu nhìu😘😘',
+    messDesc: 'Yêu bé gất nhìu❤.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/'
+    messLink: 'https://www.facebook.com'
